3a94dec38f7c6d033a19728aba026c213004d7e4
SS-2020 <63364168+SS-2020@users.noreply.github.com>
2020-08-20 14:59:32 +0530

    Add files via upload

---
+++ j1.js
@@ -0,0 +1,41 @@
+let bold=0;
+let ital=0;
+
+
+function Bold(){
+	document.execCommand("bold");
+	if(!bold)
+	{	
+		bold=1;
+		document.querySelector('#bold').style.fontWeight="bold";
+	}
+	else
+	{
+		bold=0;
+		document.querySelector('#bold').style.fontWeight="normal";
+	}
+}
+function Italic(){
+	document.execCommand("italic");
+	if(!ital)
+	{
+		document.querySelector('#ital').style.fontWeight="bold";
+		ital=1;
+	}
+	else
+	{
+		document.querySelector('#ital').style.fontWeight="normal";
+		ital=0;
+	}
+}
+function fonttype(selectTag) {
+	listtype = selectTag.options[selectTag.selectedIndex].text;
+	document.execCommand('fontname', false, listtype);
+}
+function fontfunction(selectTag) {
+	listfont = selectTag.options[selectTag.selectedIndex].value;
+	document.execCommand('fontsize', false, listfont);
+}
+function formatDoc(sCmd, sValue) {
+  document.execCommand(sCmd, false, sValue); 
+}
